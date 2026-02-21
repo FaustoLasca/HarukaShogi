790747739b5f881952d155368678d1118963de87
Fausto <fausto.lasca@gmail.com>
2026-02-21 19:34:55 +0100

    implemented Thread logic

--- searchengine/src/main.cpp
+++ searchengine/src/main.cpp
@@ -12,27 +12,53 @@
 #include "search.h"
 #include "movepicker.h"
 #include "history.h"
+#include "thread.h"
 
 using namespace harukashogi;
+
+
+class TestThread : public Thread {
+    public:
+        TestThread(int id) : Thread(id) {}
+
+    protected:
+        void search() override {
+            std::cout << "Searching..." << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            std::cout << "Search finished" << std::endl;
+        }
+};
 
 
 int main() {
     init();
 
-    TTable tt;
-    Worker worker(tt);
+    // TTable tt;
+    // Worker worker(tt);
 
-    worker.set_position("ln1g3nl/2s1k1gsp/3ppp1p1/p1p3p1P/1P7/PG1PR4/2NKPPP1L/6S2/L+pb2G1N1 w RBPPs 60");
-    worker.timeLimit = chr::milliseconds(1000);
+    // worker.set_position("ln1g3nl/2s1k1gsp/3ppp1p1/p1p3p1P/1P7/PG1PR4/2NKPPP1L/6S2/L+pb2G1N1 w RBPPs 60");
+    // worker.timeLimit = chr::milliseconds(1000);
 
-    worker.start_searching();
+    // worker.start_searching();
 
-    std::cout << "Depth:      " << worker.depth << std::endl;
-    std::cout << "Evaluation: " << worker.eval << std::endl;
-    std::cout << "Best move:  " << worker.bestMove.to_string() << std::endl;
-    std::cout << "Node count: " << worker.nodeCount << std::endl;
-    std::cout << "TT Stats" << std::endl;
-    tt.print_stats();
+    // std::cout << "Depth:      " << worker.depth << std::endl;
+    // std::cout << "Evaluation: " << worker.eval << std::endl;
+    // std::cout << "Best move:  " << worker.bestMove.to_string() << std::endl;
+    // std::cout << "Node count: " << worker.nodeCount << std::endl;
+    // std::cout << "TT Stats" << std::endl;
+    // tt.print_stats();
+
+    TestThread thread(0);
+    std::cout << "Thread created" << std::endl;
+
+    thread.start_searching();
+    std::cout << "Thread started" << std::endl;
+
+    thread.wait_search_finished();
+    std::cout << "Thread finished" << std::endl;
+
+    thread.exit();
+    std::cout << "Thread exited" << std::endl;
 
     return 0;
 }
---
+++ searchengine/src/thread.h
@@ -0,0 +1,62 @@
+#ifndef THREAD_H
+#define THREAD_H
+
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+
+namespace harukashogi {
+
+
+class Thread {
+    public:
+        Thread(size_t id) : threadId(id), thread(&Thread::idle_loop, this) {}
+        ~Thread();
+
+        // the idle loop is the main loop of the thread
+        // waits for the thread to be signaled to start searching
+        void idle_loop();
+
+        // non blocking functions to control the thread
+        void start_searching();
+        void abort_search();
+        void exit();
+        // blocking function to wait for the thread to finish searching
+        void wait_search_finished();
+
+    protected:
+        // the search function is the function that does the work of the thread, called inside the
+        // idle loop.
+        virtual void search() = 0;
+
+        // checks if the searchFlag has been set to false.
+        bool is_search_aborted() const;
+
+        // used by the Worker, so needs to be protected.
+        size_t threadId;
+
+    private:
+        std::thread thread;
+        // mutex and condition variable to synchronize the thread
+        std::mutex mutex;
+        std::condition_variable cv;
+
+        // flags to control the thread
+        // search: set to true when the thread is signaled to start searching, set to false when 
+        //         the thread is signaled to stop searching.
+        // exit: when the flag is set, the thread will exit the idle loop.
+        // searching: set to true when the thread is searching, set to false when the thread is not 
+        //            searching. This is used to wait for the thread to finish searching.
+        // 
+        // read without holding the mutex, needs to be atomic.
+        std::atomic<bool> searchFlag = false;
+        // always read and written while holding the mutex, no need to be atomic.
+        bool exitFlag = false;
+        bool searchingFlag = false;
+};
+
+
+} // namespace harukashogi
+
+#endif // THREAD_H
