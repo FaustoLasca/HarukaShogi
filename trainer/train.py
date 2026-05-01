d4306c6f4b0dbb16afbb8db670480d8a07b89a52
Fausto <fausto.lasca@gmail.com>
2026-05-01 13:26:41 +0200

    trained new network with binp data

--- trainer/train.py
+++ trainer/train.py
@@ -10,7 +10,7 @@ from dataset import NNUEIterableDataset
 
 device = "cuda"
 LAMBDA = 0.5
-EPOCHS = 200
+EPOCHS = 100
 
 
 def crossentropy_loss(output, target):
@@ -22,21 +22,21 @@ def crossentropy_loss(output, target):
 
 model = NNUEModel().to(device)
 train_dataloader = DataLoader(
-    NNUEIterableDataset("data/nnue/train", batch_size=16384, random_hflip=True),
+    NNUEIterableDataset("data/nnue/dataset_v0/train", batch_size=16384, random_hflip=True),
     batch_size=None,
     num_workers=16,
     persistent_workers=True,
     prefetch_factor=4,
     pin_memory=True,
 )
-val_dataloader = DataLoader(
-    NNUEIterableDataset("data/nnue/val", batch_size=16384, random_hflip=False, shuffle=False),
-    batch_size=None,
-    num_workers=16,
-    persistent_workers=True,
-    prefetch_factor=4,
-    pin_memory=True,
-)
+# val_dataloader = DataLoader(
+#     NNUEIterableDataset("data/nnue/val", batch_size=16384, random_hflip=False, shuffle=False),
+#     batch_size=None,
+#     num_workers=16,
+#     persistent_workers=True,
+#     prefetch_factor=4,
+#     pin_memory=True,
+# )
 optimizer = AdamW(model.parameters(), lr=1e-3, weight_decay=1e-2)
 scheduler = ExponentialLR(optimizer, gamma=0.1**(1/EPOCHS))
 
@@ -61,32 +61,34 @@ for epoch in range(EPOCHS):
         train_losses.append(loss.detach())
 
     
-    for batch in val_dataloader:
-        b, w, s, r, t = [tensor.to(device) for tensor in batch]
+    # for batch in val_dataloader:
+    #     b, w, s, r, t = [tensor.to(device) for tensor in batch]
 
-        s = s/(127*64)
+    #     s = s/(127*64)
         
-        with torch.no_grad():
-            output = torch.sigmoid(model(b, w, t))
-        target = (LAMBDA*torch.sigmoid(s*4) + (1 - LAMBDA)*r).unsqueeze(-1)
+    #     with torch.no_grad():
+    #         output = torch.sigmoid(model(b, w, t))
+    #     target = (LAMBDA*torch.sigmoid(s*4) + (1 - LAMBDA)*r).unsqueeze(-1)
 
-        loss = crossentropy_loss(output, target)
-        val_losses.append(loss.detach())
+    #     loss = crossentropy_loss(output, target)
+    #     val_losses.append(loss.detach())
     
     train_loss = torch.stack(train_losses).mean().item()
-    val_loss = torch.stack(val_losses).mean().item()
+    # val_loss = torch.stack(val_losses).mean().item()
+    # print(f"Epoch {epoch}, \tLR: {scheduler.get_last_lr()[0]:.4e}, \t" \
+    #       f"Train loss: {train_loss:.4f}, \tVal loss: {val_loss:.4f}")
     print(f"Epoch {epoch}, \tLR: {scheduler.get_last_lr()[0]:.4e}, \t" \
-          f"Train loss: {train_loss:.4f}, \tVal loss: {val_loss:.4f}")
+          f"Train loss: {train_loss:.4f}")
     train_losses = []
-    val_losses = []
+    # val_losses = []
 
-    if val_loss < min_val_loss:
-        min_val_loss = val_loss
-        model.weights_to_bin("searchengine/bin/nnue/AdamW_acc32_46M.bin")
-        print("Model saved to searchengine/bin/nnue/AdamW_acc32_46M.bin")
+    # if val_loss < min_val_loss:
+        # min_val_loss = val_loss
+    model.weights_to_bin("searchengine/bin/nnue/AdamW_acc32_300M.bin")
+    # print("Model saved to searchengine/bin/nnue/AdamW_acc32_300M.bin")
 
 
     scheduler.step()
 
 del train_dataloader
-del val_dataloader
+# del val_dataloader
